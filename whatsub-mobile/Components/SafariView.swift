418fd05ec1936b9219447224c2235fa726a49a5a
家兴 任 <2216681472@qq.com>
2026-05-22 14:45:03 +0800

    feat(ios/corpus): YouTubeEmbedView startSeconds + SafariView

---
+++ whatsub-mobile/Components/SafariView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import SafariServices
+
+/// Opens a URL in an in-app Safari sheet (SFSafariViewController).
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    func updateUIViewController(_ vc: SFSafariViewController, context: Context) {}
+}
