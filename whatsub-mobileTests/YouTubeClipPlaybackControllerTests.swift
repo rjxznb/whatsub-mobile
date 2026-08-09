6d35383a671ba9c58ac20e930f8529622bc86d83
家兴 任 <2216681472@qq.com>
2026-08-09 18:00:59 +0800

    test: reproduce pre-ready youtube clip loss

--- whatsub-mobileTests/YouTubeClipPlaybackControllerTests.swift
+++ whatsub-mobileTests/YouTubeClipPlaybackControllerTests.swift
@@ -39,6 +39,15 @@ final class YouTubeClipPlaybackControllerTests: XCTestCase {
         XCTAssertFalse(controller.isPlaying)
     }
 
+    func testClipQueuedBeforeReadyIsDeliveredExactlyOnceAfterReady() {
+        var delivery = YouTubeClipCommandDeliveryState()
+        let command = YouTubeClipPlaybackCommand.play(start: 1, end: 2, rate: 0.75)
+
+        XCTAssertNil(delivery.queue(command))
+        XCTAssertEqual(delivery.markReady(), command)
+        XCTAssertNil(delivery.markReady())
+    }
+
     func testClipJavaScriptContainsBoundaryAndPauseLogic() {
         let play = YouTubeClipPlaybackCommand.play(start: 1, end: 2, rate: 0.75)
         let script = YouTubeEmbedView.clipJavaScript(for: play) ?? ""
